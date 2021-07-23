e1204cf64b3bf92143bc57098833d33cdd76f669
chexPK <chexpk@gmail.com>
2021-07-24 00:41:21 +0300

    started create particle

---
+++ Assets/Scripts/CameraChanger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public GameObject [] cameras;
+    public int activeCamera;
+
+    void Start()
+    {
+        //либо метод загрузки или иная камера
+        activeCamera = 0;
+
+        foreach(GameObject cam in cameras)
+        {
+            cam.SetActive(false);
+        }
+
+        cameras[activeCamera].SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+            ChangeCamera();
+    }
+
+    void ChangeCamera()
+    {
+        cameras[activeCamera].SetActive(false);
+
+        if(activeCamera + 1 >= cameras.Length)
+            activeCamera = 0;
+        else
+            activeCamera += 1;
+
+        cameras[activeCamera].SetActive(true);
+    }
+}
--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public AxleInfo[] carAxis = new AxleInfo[2];
+    public WheelCollider[] wheelColliders;
 
     public float carSpeed;
     public float steerAngle;
     public Transform centerOfMass;
+    public float nitroPower;
 
-    private float horInput;
-    private float vertInput;
+    [Range(0,1)]
+    public float steerHelpValue = 0;
 
+    float horInput;
+    float vertInput;
+    bool isOnground;
+    float lastYRotation;
     Rigidbody rb;
 
     private void Awake()
@@ -28,7 +34,10 @@ public class CarController : MonoBehaviour
     {
         horInput = Input.GetAxis("Horizontal");
         vertInput = Input.GetAxis("Vertical");
+        CheckOnGround();
         Accelerate();
+        NitroManager();
+        SteerHelper();
     }
 
     void Accelerate()
@@ -58,5 +67,42 @@ public class CarController : MonoBehaviour
 
         visWheel.position = position;
         visWheel.rotation = rotation;
+    }
+
+    void SteerHelper()
+    {
+        if(!isOnground) return;
+
+        if(Mathf.Abs(transform.rotation.eulerAngles.y - lastYRotation) < 10f)
+        {
+            float turnAdjust = (transform.rotation.eulerAngles.y - lastYRotation) * steerHelpValue;
+            Quaternion rotateHelp = Quaternion.AngleAxis(turnAdjust, Vector3.up);
+            rb.velocity = rotateHelp * rb.velocity;
+        }
+        lastYRotation = transform.rotation.eulerAngles.y;
+    }
+
+    void CheckOnGround()
+    {
+        isOnground = true;
+        foreach (var wheelCol in wheelColliders)
+        {
+            if (!wheelCol.isGrounded)
+            {
+                isOnground = false;
+            }
+        }
+    }
+
+    void NitroManager()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && vertInput > 0.01f)
+        {
+            rb.AddForce(transform.forward * nitroPower);
+        }
+        else
+        {
+
+        }
     }
 }
